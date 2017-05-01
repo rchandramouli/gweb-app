e1b1f5091b971bd47a3ec46a120277e101e124b6
Ramasamy Chandramouli <rchandramouli@live.com>
2017-05-01 17:43:27 +0530

    Update MySQL schema setup based on split tables
    Signed-off-by: Ramasamy Chandramouli <rchandramouli@live.com>

--- setup/mysql_schema.c
+++ setup/mysql_schema.c
@@ -17,24 +17,54 @@
 #include <gweb/mysqldb_log.h>
 
 /* SQL Macros */
-#define DROP_TABLE_AppUsers						\
-    "DROP TABLE IF EXISTS AppUsers"
+#define DROP_TABLE_UserRegInfo                  \
+    "DROP TABLE IF EXISTS UserRegInfo"
 
-#define CREATE_TABLE_AppUsers						\
-    "CREATE TABLE AppUsers (FirstName VARCHAR(20), LastName VARCHAR(20), " \
-    "Email VARCHAR(40) NOT NULL UNIQUE, Phone CHAR(10) NOT NULL, Address1 VARCHAR(40), " \
-    "Address2 VARCHAR(40), Address3 VARCHAR(40), Country VARCHAR(20), " \
-    "State VARCHAR(20), Pincode VARCHAR(6), Password VARCHAR(64), "	\
-    "RegisteredOn DATE, LastUpdatedOn DATE, Validated BOOLEAN," \
-    "PRIMARY KEY (`Phone`))"
+#define CREATE_TABLE_UserRegInfo                                        \
+    "CREATE TABLE UserRegInfo (UID VARCHAR(16) NOT NULL UNIQUE, "       \
+    "FirstName VARCHAR(20), LastName VARCHAR(20), "                     \
+    "Email VARCHAR(40) NOT NULL UNIQUE, StartDate DATETIME, "           \
+    "EndDate DATETIME, Password VARCHAR(64), Validated BOOLEAN, "       \
+    "UIDVersion INTEGER, DataVersion INTEGER, AvatarURL VARCHAR(100), " \
+    "MobileIP VARCHAR(45), PRIMARY KEY (`UID`))"
+
+#define DROP_TABLE_UserAddress                  \
+    "DROP TABLE IF EXISTS UserAddress"
+
+#define CREATE_TABLE_UserAddress                                        \
+    "CREATE TABLE UserAddress (UID VARCHAR(16) NOT NULL, "              \
+    "AddressType VARCHAR(10), Address1 VARCHAR(40), Address2 VARCHAR(40), " \
+    "Address3 VARCHAR(40), State VARCHAR(20), Pincode VARCHAR(6), "     \
+    "Country VARCHAR(20))"
+
+#define DROP_TABLE_UserPhone                    \
+    "DROP TABLE IF EXISTS UserPhone"
+
+#define CREATE_TABLE_UserPhone                                  \
+    "CREATE TABLE UserPhone (UID VARCHAR(16) NOT NULL, "        \
+    "PhoneType VARCHAR(10), Phone VARCHAR(10) NOT NULL)"
+
+#define DROP_TABLE_UserSocialNetwork            \
+    "DROP TABLE IF EXISTS UserSocialNetwork"
+
+#define CREATE_TABLE_UserSocialNetwork                                  \
+    "CREATE TABLE UserSocialNetwork (UID VARCHAR(16) NOT NULL, "        \
+    "NetworkType VARCHAR(10) NOT NULL, NetworkHandle VARCHAR(40) NOT NULL)"
 
 /*
  * List of queries to run through to setup tables the first time. Add
  * user/db creation as well to the list.
  */
 static const char *mysql_query_list[] = {
-    DROP_TABLE_AppUsers,
-    CREATE_TABLE_AppUsers,
+    DROP_TABLE_UserRegInfo,
+    DROP_TABLE_UserAddress,
+    DROP_TABLE_UserPhone,
+    DROP_TABLE_UserSocialNetwork,
+
+    CREATE_TABLE_UserRegInfo,
+    CREATE_TABLE_UserAddress,
+    CREATE_TABLE_UserPhone,
+    CREATE_TABLE_UserSocialNetwork,
 };
 
 int main (int argc, char *argv[])
