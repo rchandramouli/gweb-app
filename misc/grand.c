5a834322c690135acb7769b7071435a6d042b190
Ramasamy Chandramouli <rchandramouli@live.com>
2017-05-01 18:06:11 +0530

    Update grand.c snippet to produce base-64 shuffle
    Signed-off-by: Ramasamy Chandramouli <rchandramouli@live.com>

--- misc/grand.c
+++ misc/grand.c
@@ -5,14 +5,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-static char *base62 =
+#define MAX_SYMBOLS  (64)
+
+static char *base64 =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.";
     
 int main (int argc, char *argv[])
 {
     int fd, nr;
     uint32_t random;
-    char random_base62[63], ch;
+    char random_base64[MAX_SYMBOLS+1], ch;
 
     fd = open("/dev/urandom", O_RDONLY);
     if (fd < 0) {
@@ -29,18 +31,18 @@ int main (int argc, char *argv[])
     }
     printf("};\n");
 
-    /* Generate Base62 random string */
-    for (nr = 0; nr < 62; nr++)
-        random_base62[nr] = base62[nr];
-    for (nr = 0; nr < 61; nr++) {
+    /* Generate Base64 random string */
+    for (nr = 0; nr < MAX_SYMBOLS; nr++)
+        random_base64[nr] = base64[nr];
+    for (nr = 0; nr < MAX_SYMBOLS-1; nr++) {
         read(fd, &random, sizeof(random));
-        random = nr + (random % (62 - nr));
-        ch = random_base62[nr];
-        random_base62[nr] = random_base62[random];
-        random_base62[random] = ch;
+        random = nr + (random % (MAX_SYMBOLS - nr));
+        ch = random_base64[nr];
+        random_base64[nr] = random_base64[random];
+        random_base64[random] = ch;
     }
-    random_base62[62] = '\0';
-    printf("\n[%s]\n", random_base62);
+    random_base64[MAX_SYMBOLS] = '\0';
+    printf("\n[%s]\n", random_base64);
     
     close(fd);
 
